02dfd24035be5becffa1ff9cc9e46e6846168b99
JPalakapilly <jjp126@gmail.com>
2017-09-26 11:51:58 -0700

    Connect method now checks to see if already connected.

--- node.py
+++ node.py
@@ -1,23 +1,27 @@
-class MPNode(object):
+class MPnode(object):
     """docstring for MPNode."""
 
     def __init__(self, name, relations=[]):
         #super(MPNode, self).__init__()
         self.name = name
-        self.relationList = relations
-        self.numRelations = len(self.relationList)
+        self.relationList = []
+        self.numRelations = 0
+        for relation in relations:
+            self.connect(relation)
+
 
     def connect(self, relation):
         if (not isinstance(relation,MPrelation)):
             print("Node must be connected to a relation")
-        else:
-        self.relationList.append(relation)
-        self.numRelations += 1
+        elif (relation not in self.relationList):
+            self.relationList.append(relation)
+            self.numRelations += 1
+            relation.connect(self)
 
     def getPath(self):
         for relation in self.relationList:
             print(self.name + " can be found using " + relation.name)
-            relation.getNodes()
+            relation.getReqs(self)
 
 
 
@@ -27,40 +31,44 @@ class MPrelation(object):
 
     def __init__(self, name, nodes=[]):
         #super(MPnode, self).__init__()
-        self.nodeList = nodes
         self.name = name;
-        self.numNodes = len(self.nodeList)
+        self.nodeList = []
+        self.numNodes = 0
+        for node in nodes:
+            self.connect(node)
+
 
     def connect(self, node):
         if (not isinstance(node,MPnode)):
             print("node must be connected to a relation")
-            raise TypeError
-        if (node not in self.nodeList):
+        elif (node not in self.nodeList):
             self.nodeList.append(node)
             self.numNodes += 1
+            node.connect(self)
 
-    def getReqs(rootNode):
+    def getReqs(self,rootNode):
+        needed = []
         for node in self.nodeList:
             if not node.name == rootNode.name:
-                print node.name
+                needed.append(node.name)
+        print("Needed variables:")
+        print(needed)
+        print("")
 
 if __name__ == "__main__":
-    a = MPnode("Pressure")
-    b = MPnode("Volume")
-    c = MPrelation("Ideal Gas Law",nodes=[a,b])
-    a.connect(c)
-    c.connect(a)
-    c.connect(b)
-    print(a.numRelations)
-    print(b.numRelations)
-    print(c.numNodes)
-    a.getPath()
-
-class MPnode(object):
-    """docstring for MPnode."""
-
-
-    def getPath(self):
-        for relation in self.relationList:
-            print(self.name + " can be found using " + relation.name)
-            relation.getReqs(self.name)
+    P = MPnode("Pressure")
+    V = MPnode("Volume")
+    T = MPnode("Temperature")
+    n = MPnode("Num Moles")
+    IGL = MPrelation("Ideal Gas Law",nodes=[P,V,n,T])
+    P.connect(IGL)
+    IGL.connect(V)
+    T.connect(IGL)
+    Eint = MPrelation("Equation to find internal energy")
+    U = MPnode("Internal Energy", [Eint])
+    n.connect(Eint)
+    Eint.connect(T)
+    print(P.numRelations)
+    print(V.numRelations)
+    print(IGL.numNodes)
+    T.getPath()
